d3e09d38f1f5e48e6380328a1357b7ae45f3f7ec
Shaun Murphy <Shaun9421@outlook.com>
2022-10-16 10:51:35 +0800

    add pom

---
+++ src/test/java/com/shaun/fansQuary/FansQuaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.fansQuary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FansQuaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
